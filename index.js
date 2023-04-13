a8451b24dad42e02623cda4b52de6e40dc184b37
lbrlab <contato@lbrtelecom.com.br>
2023-04-13 14:24:21 -0300

    Primeiro commit

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+const bot = require("./functions/bot.js")
 newFunction();
 
 function newFunction() {
-    const TelegramBot = require('node-telegram-bot-api');
-    const token = 'COLOQUE_AQUI_SEU_TOKEN';
-    const bot = new TelegramBot(token, { polling: true });
+
     bot.on('message', (msg) => {
         bot.sendMessage(msg.chat.id, 'Olá! Obrigado por entrar em contato!');
 
