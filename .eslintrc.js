a393e7146466622024e9760c608209a490cdbc9a
lbrlab <contato@lbrtelecom.com.br>
2023-04-13 14:16:56 -0300

    Primeiro commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
     "env": {
-        "browser": true,
+        "browser": false,
         "commonjs": true,
-        "es2021": true
+        "es2021": false
     },
     "overrides": [
     ],
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-/* eslint linebreak-style: ["error", "windows"]*/
-const TelegramBot = require('node-telegram-bot-api');
-const token = 'COLOQUE_AQUI_SEU_TOKEN';
-const bot = new TelegramBot(token, {polling: true});
-bot.on('message', (msg) => {
-  bot.sendMessage(msg.chat.id, 'Olá! Obrigado por entrar em contato!');
-// eslint-disable-next-line eol-last
-});
+newFunction();
+
+function newFunction() {
+    const TelegramBot = require('node-telegram-bot-api');
+    const token = 'COLOQUE_AQUI_SEU_TOKEN';
+    const bot = new TelegramBot(token, { polling: true });
+    bot.on('message', (msg) => {
+        bot.sendMessage(msg.chat.id, 'Olá! Obrigado por entrar em contato!');
+
+    });
+}
